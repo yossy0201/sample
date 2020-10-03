2ea27281cae4f96068299403c0e1a2f2caad45d5
yossy0201 <yossy0201msk@gmail.com>
2020-10-03 17:46:47 +0900

    name-number

--- Downloads/java-basic-master/src/practice/EqualsPractice.java
+++ Downloads/java-basic-master/src/practice/EqualsPractice.java
@@ -2,5 +2,13 @@ package practice;
 
 public class EqualsPractice {
     public static void main(String[] args) {
+        String name = "きのこ";
+        System.out.println("Hello " + name);
+
+        String number = "1" + "2";
+        System.out.println(number);
+
+        int numberInt = 1 + 2;
+        System.out.println(numberInt);
     }
 }
